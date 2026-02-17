fc71e7399f1ec9043ef08b797e173513fe32ae3f
alexlison <alexlison910@gmail.com>
2026-02-17 21:27:07 +0530

    Category Url fixed

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 const Categories = ({ token }) => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(true);
-  const BASE_URL = `${import.meta.env.VITE_API_URL}`;
+  const BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
   const categoryIcons = {
     'Tools': <Wrench className="w-6 h-6" />,
